7c67dcc11d65d148b34078ea93013c1520023aa4
cuifangfang <15965873315@163.com>
2019-11-26 11:51:09 +0800

    1.0

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -2,10 +2,16 @@
 
 namespace app\admin\controller;
 
+use app\common\controller\BaseController;
 use think\Controller;
-class Index extends Controller{
+class Index extends BaseController {
 
     public function index(){
         return $this->fetch();
+    }
+
+    public function add(){
+        return $this->fetch();
+
     }
 }
---
+++ runtime/temp/33e44b080a1e1f6086b4c983c152d7c2.php
@@ -0,0 +1,42 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:5:{s:82:"/Users/cuifangfang/www/thinkphp5.0/public/../application/admin/view/index/add.html";i:1574735661;s:72:"/Users/cuifangfang/www/thinkphp5.0/application/common/layout/layout.html";i:1574739834;s:72:"/Users/cuifangfang/www/thinkphp5.0/application/common/layout/header.html";i:1574740236;s:70:"/Users/cuifangfang/www/thinkphp5.0/application/common/layout/left.html";i:1574739636;s:72:"/Users/cuifangfang/www/thinkphp5.0/application/common/layout/footer.html";i:1574739813;}*/ ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/x-icon" href="/admin/image/favicon.jpeg"/>
+    <title>admin</title>
+</head>
+<body>
+<!--头部-->
+<div>
+    <?php echo $data; ?>
+    header
+</div>
+<!--头部end-->
+
+<!--内容-->
+<div>
+application/common/layout/layout.html
+<div>
+    add
+
+</div>
+</div>
+<!--内容end-->
+
+
+<!--左侧-->
+<div>
+    left
+</div>
+<!--左侧end-->
+
+<!--底部-->
+<div>
+    footer
+</div>
+<!--底部end-->
+</body>
+</html>
